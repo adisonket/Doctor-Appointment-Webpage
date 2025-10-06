1ef1ebead494db69e1dac61d4faca4a7287cddfc
Adhiphalder <adhiphalder8585@gmail.com>
2025-10-06 20:59:52 +0530

    bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import Preloader from './Home/preloader';
 import Homepage from './Home/homepage';
+import 
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 
 // import './App.css';
 
@@ -12,10 +15,17 @@ function App() {
 
   return (
     <>
-      
+      {/* <BrowserRouter>
+        <Header/>
+        <Routes>
+          <Route path="/appointmentdoctor" element={<AppointmentsDoctor/>} />
+        </Routes>
+      </BrowserRouter> */}
 
       <Preloader/>
       <Homepage/>
+
+
     </>
   );
 }
