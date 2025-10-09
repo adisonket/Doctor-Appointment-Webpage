1d3fe3cec725ae6c8bc973b6214382e73c7edf51
Sanket Adhikary <adisonket.99@gmail.com>
2025-10-09 20:29:16 +0530

    View Appointments for Patient Added

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,17 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Preloader from './Home/preloader';
 import Homepage from './Home/homepage';
 import AppointmentsDoctor from './Appointments/AppointmentsDoctor';
+import AppointmentsPatient from './Appointments/AppointmentsPatient';
 
 function App() {
   return (
     <>
-      <Preloader />
+      {/* <Preloader /> */}
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Homepage />} />
           <Route path="/appointmentdoctor" element={<AppointmentsDoctor />} />
+          <Route path="/appointmentpatient" element={<AppointmentsPatient />} />
         </Routes>
       </BrowserRouter>
     </>
