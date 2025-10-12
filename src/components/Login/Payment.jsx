a03c0c68e113b953876caac327ca5f3cdec7b27c
mriganko07 <mrigankaadhikary35@gmail.com>
2025-10-12 23:15:32 +0530

    Payment & AdminLogin Fix

--- src/components/Login/Payment.jsx
+++ src/components/Login/Payment.jsx
@@ -69,166 +69,169 @@ const Payment = () => {
     };
 
     return (
-        <div className={style.payment}>
-            <div className={style.payment_left_upper}>
-                <div className={style.credit_card}>
-                    <div className={style.credit_card_front}>
-                        <div className={style.credit_card_front_img}>
-                            <img src={mastercardLogo} className={style.credit_card_front_logo} alt="" />
-                        </div>
-                        <div className={style.credit_card_front_num}>
-                            <p>{cardNumber.replace(/(\d{4})(?=\d)/g, '$1 ')}</p>
-                            <p className={style.credit_card_front_num_sec}>
-                                <span>VALID THRU</span>
-                                <span>{cardExpiry}</span>
-                            </p>
-                        </div>
-                        <div className={style.credit_card_front_name}>
-                            <span>{cardName.toUpperCase()}</span>
-                            <div className={style.credit_card_front_name_sec}>
-                                <span>{'*'.repeat(cardCvv.length)}</span>
+
+        <div className={style.body_part}>
+            <div className={style.payment}>
+                <div className={style.payment_left_upper}>
+                    <div className={style.credit_card}>
+                        <div className={style.credit_card_front}>
+                            <div className={style.credit_card_front_img}>
+                                <img src={mastercardLogo} className={style.credit_card_front_logo} alt="" />
+                            </div>
+                            <div className={style.credit_card_front_num}>
+                                <p>{cardNumber.replace(/(\d{4})(?=\d)/g, '$1 ')}</p>
+                                <p className={style.credit_card_front_num_sec}>
+                                    <span>VALID THRU</span>
+                                    <span>{cardExpiry}</span>
+                                </p>
+                            </div>
+                            <div className={style.credit_card_front_name}>
+                                <span>{cardName.toUpperCase()}</span>
+                                <div className={style.credit_card_front_name_sec}>
+                                    <span>{'*'.repeat(cardCvv.length)}</span>
+                                </div>
                             </div>
                         </div>
-                    </div>
 
-                    <div className={style.credit_card_front_add}>
-                        <button className={style.credit_card_front_add_button}>
-                            Add New
-                            <AddAnimation></AddAnimation>
-                        </button>
-                    </div>
-                </div>
-            </div>
-
-            <div className={style.payment_middle}></div>
-
-            <div className={style.payment_left}>
-                <div className={style.payment_details}>
-                    <div className={`${style.payment_details_ammount} ${style.payment_details_att}`} style={{ marginTop: 0 }}>
-                        <span className={style.payment_font_green}>Booking Ammount</span>
-                        <span>Rs. 2800</span>
-                    </div>
-                    <div className={`${style.payment_details_tax} ${style.payment_details_att}`}>
-                        <span className={style.payment_font_green}>Total Tax</span>
-                        <span>504</span>
-                    </div>
-                    <div className={`${style.payment_details_total} ${style.payment_details_att}`}>
-                        <span className={style.payment_font_green}>Total Ammount</span>
-                        <span>Rs. 3304</span>
+                        <div className={style.credit_card_front_add}>
+                            <button className={style.credit_card_front_add_button}>
+                                Add New
+                                <AddAnimation></AddAnimation>
+                            </button>
+                        </div>
                     </div>
                 </div>
 
-                <div className={style.payment_left_between}></div>
+                <div className={style.payment_middle}></div>
 
-                <div className={style.payment_options}>
-                    <div className={style.payment_method}>
-                        <span className={style.payment_font_green}>Payment Options:</span>
-                        <div className={style.payment_options_button}>
-                            <button ref={cardBtnRef} onClick={handleCardClick} className={style.payment_button_styles}>Card</button>
-                            <button ref={upiBtnRef} onClick={handleUpiClick} className={style.payment_button_styles}>UPI</button>
+                <div className={style.payment_left}>
+                    <div className={style.payment_details}>
+                        <div className={`${style.payment_details_ammount} ${style.payment_details_att}`} style={{ marginTop: 0 }}>
+                            <span className={style.payment_font_green}>Booking Ammount</span>
+                            <span>Rs. 2800</span>
+                        </div>
+                        <div className={`${style.payment_details_tax} ${style.payment_details_att}`}>
+                            <span className={style.payment_font_green}>Total Tax</span>
+                            <span>504</span>
+                        </div>
+                        <div className={`${style.payment_details_total} ${style.payment_details_att}`}>
+                            <span className={style.payment_font_green}>Total Ammount</span>
+                            <span>Rs. 3304</span>
                         </div>
                     </div>
 
-                    <div className={style.payment_slider_container}>
-                        <div className={style.payment_slider_wrapper} ref={sliderRef}>
-                            <div className={style.payment_slide}>
-                                <form className={style.payment_options_card_form} onSubmit={handleSubmit}>
-                                    <input
-                                        type="text"
-                                        placeholder="Enter 16 Digits Card Number"
-                                        className={style.payment_options_card_inp}
-                                        maxLength="16"
-                                        pattern="\d{16}"
-                                        value={cardNumber}
-                                        onChange={(e) => setCardNumber(e.target.value.replace(/[^0-9]/g, '').slice(0, 16))}
-                                        required
-                                    />
+                    <div className={style.payment_left_between}></div>
 
-                                    <input
-                                        type="text"
-                                        placeholder="Cardholder Name"
-                                        maxLength="22"
-                                        className={style.payment_options_card_name}
-                                        value={cardName}
-                                        onChange={(e) => setCardName(e.target.value.replace(/[^a-zA-Z\s]/g, ''))}
-                                        required
-                                    />
+                    <div className={style.payment_options}>
+                        <div className={style.payment_method}>
+                            <span className={style.payment_font_green}>Payment Options:</span>
+                            <div className={style.payment_options_button}>
+                                <button ref={cardBtnRef} onClick={handleCardClick} className={style.payment_button_styles}>Card</button>
+                                <button ref={upiBtnRef} onClick={handleUpiClick} className={style.payment_button_styles}>UPI</button>
+                            </div>
+                        </div>
 
-                                    <div>
+                        <div className={style.payment_slider_container}>
+                            <div className={style.payment_slider_wrapper} ref={sliderRef}>
+                                <div className={style.payment_slide}>
+                                    <form className={style.payment_options_card_form} onSubmit={handleSubmit}>
                                         <input
                                             type="text"
-                                            name="expiry"
-                                            placeholder="MM/YYYY"
-                                            className={style.payment_options_card_date}
-                                            pattern="(0[1-9]|1[0-2])\/[0-9]{4}"
-                                            maxLength="7"
-                                            value={cardExpiry}
-                                            onChange={handleExpiryInput}
+                                            placeholder="Enter 16 Digits Card Number"
+                                            className={style.payment_options_card_inp}
+                                            maxLength="16"
+                                            pattern="\d{16}"
+                                            value={cardNumber}
+                                            onChange={(e) => setCardNumber(e.target.value.replace(/[^0-9]/g, '').slice(0, 16))}
                                             required
                                         />
+
                                         <input
-                                            type="password"
-                                            inputMode="numeric"
-                                            placeholder="CVV"
-                                            className={style.payment_options_card_cvv}
-                                            maxLength="4"
-                                            pattern="\d{3,4}"
-                                            value={cardCvv}
-                                            onChange={(e) => setCardCvv(e.target.value.replace(/\D/g, ''))}
+                                            type="text"
+                                            placeholder="Cardholder Name"
+                                            maxLength="22"
+                                            className={style.payment_options_card_name}
+                                            value={cardName}
+                                            onChange={(e) => setCardName(e.target.value.replace(/[^a-zA-Z\s]/g, ''))}
                                             required
                                         />
-                                    </div>
 
-                                    <button className={`${style.payment_button_styles} ${style.payment_checkout}`} type="submit" style={{ marginTop: 8 }}>
-                                        Checkout
-                                    </button>
-                                </form>
-                            </div>
+                                        <div className={style.mmcvv}>
+                                            <input
+                                                type="text"
+                                                name="expiry"
+                                                placeholder="MM/YYYY"
+                                                className={style.payment_options_card_date}
+                                                pattern="(0[1-9]|1[0-2])\/[0-9]{4}"
+                                                maxLength="7"
+                                                value={cardExpiry}
+                                                onChange={handleExpiryInput}
+                                                required
+                                            />
+                                            <input
+                                                type="password"
+                                                inputMode="numeric"
+                                                placeholder="CVV"
+                                                className={style.payment_options_card_cvv}
+                                                maxLength="4"
+                                                pattern="\d{3,4}"
+                                                value={cardCvv}
+                                                onChange={(e) => setCardCvv(e.target.value.replace(/\D/g, ''))}
+                                                required
+                                            />
+                                        </div>
 
-                            <div className={style.payment_slide}>
-                                <form className={style.payment_options_upi_form}>
-                                    <input type="email" placeholder="Enter your VPA" className={style.payment_options_upi_inp} />
-                                    <button className={`${style.payment_button_styles} ${style.payment_checkout}`} type="submit">
-                                        Checkout
-                                    </button>
-                                </form>
+                                        <button className={`${style.payment_button_styles} ${style.payment_checkout}`} type="submit" style={{ marginTop: 8 }}>
+                                            Checkout
+                                        </button>
+                                    </form>
+                                </div>
+
+                                <div className={style.payment_slide}>
+                                    <form className={style.payment_options_upi_form}>
+                                        <input type="email" placeholder="Enter your VPA" className={style.payment_options_upi_inp} />
+                                        <button className={`${style.payment_button_styles} ${style.payment_checkout}`} type="submit">
+                                            Checkout
+                                        </button>
+                                    </form>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
 
-            <div className={style.payment_middle}></div>
+                <div className={style.payment_middle}></div>
 
-            <div className={style.payment_right}>
-                <div className={style.patient_details}>
-                    <div className={`${style.patient_details_name} ${style.payment_right_pad}`}>
-                        <span className={style.payment_font_green}>Patient Name:</span>
-                        <span>Priya Halder</span>
+                <div className={style.payment_right}>
+                    <div className={style.patient_details}>
+                        <div className={`${style.patient_details_name} ${style.payment_right_pad}`}>
+                            <span className={style.payment_font_green}>Patient Name:</span>
+                            <span>Priya Halder</span>
+                        </div>
+                        <div className={`${style.patient_details_age} ${style.payment_right_pad}`}>
+                            <span className={style.payment_font_green}>Age:</span>
+                            <span>22 <span>(F)</span></span>
+                        </div>
                     </div>
-                    <div className={`${style.patient_details_age} ${style.payment_right_pad}`}>
-                        <span className={style.payment_font_green}>Age:</span>
-                        <span>22 <span>(F)</span></span>
-                    </div>
-                </div>
 
-                <div className={style.appoinment_details}>
-                    <div className={`${style.appoinment_details_date} ${style.payment_right_pad}`}>
-                        <span className={style.payment_font_green}>Appoinment Date:</span>
-                        <span>22/07/2025</span>
+                    <div className={style.appoinment_details}>
+                        <div className={`${style.appoinment_details_date} ${style.payment_right_pad}`}>
+                            <span className={style.payment_font_green}>Appoinment Date:</span>
+                            <span>22/07/2025</span>
+                        </div>
+                        <div className={`${style.appoinment_details_time} ${style.payment_right_pad}`}>
+                            <span className={style.payment_font_green}>Time:</span>
+                            <span>11:00 AM</span>
+                        </div>
                     </div>
-                    <div className={`${style.appoinment_details_time} ${style.payment_right_pad}`}>
-                        <span className={style.payment_font_green}>Time:</span>
-                        <span>11:00 AM</span>
-                    </div>
-                </div>
 
-                <div className={style.doctor_details}>
-                    <div className={`${style.doctor_details_name} ${style.payment_right_pad}`}>
-                        <span className={style.payment_font_green}>Doctor Name:</span>
-                        <div className={style.doctor_details_name_sec}>
-                            <span>Dr. Mohammad Singh Dubey</span>
-                            <span>MBBS(Cal), MD(Dehli), DM(Cardio)</span>
+                    <div className={style.doctor_details}>
+                        <div className={`${style.doctor_details_name} ${style.payment_right_pad}`}>
+                            <span className={style.payment_font_green}>Doctor Name:</span>
+                            <div className={style.doctor_details_name_sec}>
+                                <span>Dr. Mohammad Singh Dubey</span>
+                                <span>MBBS(Cal), MD(Dehli), DM(Cardio)</span>
+                            </div>
                         </div>
                     </div>
                 </div>
